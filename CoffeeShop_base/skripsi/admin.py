bc16736d0554fba61435f4fd37f051f092140647
Agung <108864919+DIGTIZEL@users.noreply.github.com>
2024-01-25 11:58:38 +0700

    Delete CoffeeShop_base directory

--- CoffeeShop_base/skripsi/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import CoffeeShop, Riwayat
-
-# Register your models here.
-
-admin.site.register(CoffeeShop)
-admin.site.register(Riwayat)
--- CoffeeShop_base/skripsi/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SkripsiConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "skripsi"
